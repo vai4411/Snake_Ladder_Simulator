0ed81100bcc75a03799073c600d1dfc5c91b5afd
root <root@localhost.localdomain>
2020-04-08 21:18:15 +0000

    Modified

--- Snake_Ladder_Simulator.sh
+++ Snake_Ladder_Simulator.sh
@@ -15,9 +15,10 @@ position_of_player=$INITIAL_POSITION
 flip=0
 option=0
 number_of_dice_roll=0
+count=0
 player1=0
 player2=0
-count=0
+flag=0
 
 declare -a arr
 
@@ -28,7 +29,7 @@ function roll_Dice() {
 }
 
 function check_Option() {
-	roll_Dice 
+	roll_Dice
 	case $option in
 		$SNAKE)
 			position_of_player=$(($position_of_player - $flip))
@@ -43,33 +44,54 @@ function check_Option() {
 }
 
 function board() {
+check_Option
+      if [ $position_of_player -lt $INITIAL_POSITION ]
+      then
+         position_of_player=$INITIAL_POSITION
+         elif [ $position_of_player -gt $FINAL_POSITION ]
+            then
+               position_of_player=$(($position_of_player - $flip))
+            fi
+            arr[$count]=$position_of_player
+            count=$(($count + 1))
+
+}
+
+function win() {
 	while [ $position_of_player -ne $FINAL_POSITION ] || [ $position_of_player -gt $FINAL_POSITION ]
 	do
-		check_Option 
-		if [ $position_of_player -lt $INITIAL_POSITION ]
+		if [ $flag -eq 0 ]
 		then
-			position_of_player=$INITIAL_POSITION
-			elif [ $position_of_player -gt $FINAL_POSITION ]
-				then
-					position_of_player=$(($position_of_player - $flip))
-				fi
-				arr[$count]=$position_of_player
-				count=$(($count + 1))
+			flag=1
+			position_of_player=$player1
+			board
+			player1=$position_of_player
+		else
+			flag=0
+			position_of_player=$player2
+			board
+			player2=$position_of_player
+		fi
 	done
+	flag=0
 }
 
 function position_of_player() {
-	board
+	win
 	for ((position=0 ; position<$number_of_dice_roll ; position++))
 	do
-		echo "Turn $(($position + 1)) : Position ${arr[$position]}"
+		if [ $flag -eq 0 ]
+		then
+			flag=1
+			echo "Turn $(($position + 1)) : Player1 : Position ${arr[$position]}"
+		else
+			flag=0
+			echo "Turn $(($position + 1)) : Player2 : Position ${arr[$position]}"
+		fi
 	done
 }
 
 position_of_player
-player1=$number_of_dice_roll
-position_of_player
-player2=$number_of_dice_roll
 if [ $player1 -gt $player2 ]
 then
 	echo "Player1 wins..."
