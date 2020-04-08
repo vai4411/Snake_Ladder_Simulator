131f0dc6f41b4b0feec0240dac274540159c719f
root <root@localhost.localdomain>
2020-04-08 19:50:51 +0000

    ADDED: Play Game With Two Player

--- Snake_Ladder_Simulator.sh
+++ Snake_Ladder_Simulator.sh
@@ -11,10 +11,12 @@ SNAKE=1
 LADDER=2
 
 #Variable
-position_of_player1=$INITIAL_POSITION
+position_of_player=$INITIAL_POSITION
 flip=0
 option=0
 number_of_dice_roll=0
+player1=0
+player2=0
 count=0
 
 declare -a arr
@@ -26,32 +28,32 @@ function roll_Dice() {
 }
 
 function check_Option() {
-	roll_Dice
+	roll_Dice 
 	case $option in
 		$SNAKE)
-			position_of_player1=$(($position_of_player1 - $flip))
+			position_of_player=$(($position_of_player - $flip))
 			;;
 		$LADDER)
-			position_of_player1=$(($position_of_player1 + $flip))
+			position_of_player=$(($position_of_player + $flip))
 			;;
 		*)
-			position_of_player1=$position_of_player1
+			position_of_player=$position_of_player
          ;;
 		esac
 }
 
 function board() {
-	while [ $position_of_player1 -ne $FINAL_POSITION ] || [ $position_of_player1 -gt $FINAL_POSITION ]
+	while [ $position_of_player -ne $FINAL_POSITION ] || [ $position_of_player -gt $FINAL_POSITION ]
 	do
-		check_Option
-		if [ $position_of_player1 -lt $INITIAL_POSITION ]
+		check_Option 
+		if [ $position_of_player -lt $INITIAL_POSITION ]
 		then
-			position_of_player1=$INITIAL_POSITION
-			elif [ $position_of_player1 -gt $FINAL_POSITION ]
+			position_of_player=$INITIAL_POSITION
+			elif [ $position_of_player -gt $FINAL_POSITION ]
 				then
-					position_of_player1=$(($position_of_player1 - $flip))
+					position_of_player=$(($position_of_player - $flip))
 				fi
-				arr[$count]=$position_of_player1
+				arr[$count]=$position_of_player
 				count=$(($count + 1))
 	done
 }
@@ -63,4 +65,14 @@ function position_of_player() {
 		echo "Turn $(($position + 1)) : Position ${arr[$position]}"
 	done
 }
+
 position_of_player
+player1=$number_of_dice_roll
+position_of_player
+player2=$number_of_dice_roll
+if [ $player1 -gt $player2 ]
+then
+	echo "Player1 wins..."
+else
+	echo "Player2 wins..."
+fi
