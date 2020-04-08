7dd75d9e5babe6367d29ec61ff9ecffb45c18862
root <root@localhost.localdomain>
2020-04-08 17:30:48 +0000

    ADDED: Player checks there are NO Play, Ladder or Snake Option

--- Snake_Ladder_Simulator.sh
+++ Snake_Ladder_Simulator.sh
@@ -6,14 +6,31 @@ echo "Welcome to Snake Ladder Simulator"
 #Constant
 INITIAL_POSITION=0
 FINAL_POSITION=100
+NO_PLAY=0
+SNAKE=1
+LADDER=2
 
 #Variable
-position_of_player1=0
-dice=0
+position_of_player1=$INITIAL_POSITION
+flip=0
+option=0
 
 function roll_Dice() {
 	flip=$((RANDOM % 6 + 1))
-	dice=$flip
+	option=$((RANDOM % 3))
 }
-roll_Dice
-echo $dice
+
+function check_Option() {
+	roll_Dice
+	case $option in
+		$SNAKE)
+					position_of_player1=$(($position_of_player1 - $flip))
+					;;
+		$LADDER)
+					position_of_player1=$(($position_of_player1 + $flip))
+					;;
+		*)
+					position_of_player1=$position_of_player1
+               ;;
+		esac
+}
